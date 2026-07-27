7a8c1bfa564f0743f505741caf542f4b15ffd1a3
Karima <karimanr12@gmail.com>
2026-07-27 14:27:18 +0700

    ganti judul kasir

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Art Coffee - POS Kasir</title>
+    <title>ZCoffee Hening - POS Kasir</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
